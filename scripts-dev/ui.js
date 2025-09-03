e6b449f8da9f70094f45e0b9f12a352f6132e2a0
Aweaver0608 <aweaver0608@gmail.com>
2025-09-03 13:55:26 -0400

    Fix: Export triggerConfetti from ui.js

--- scripts-dev/ui.js
+++ scripts-dev/ui.js
@@ -436,7 +436,7 @@ async function handleFeelingSelection(event) { // Ensure this function is async
     }));
 }
 
-function triggerConfetti() {
+export function triggerConfetti() {
     const confettiCount = 100;
     const colors = ['#f00', '#0f0', '#00f', '#ff0', '#0ff', '#f0f'];
 
